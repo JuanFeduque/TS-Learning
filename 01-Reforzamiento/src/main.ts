f85fa7378e3a6c5f11a6b82503f3217c2c9d7d4b
JuanFeduque <133066813+JuanFeduque@users.noreply.github.com>
2026-09-22 21:12:23 -0500

    pruebas

--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
-import './bases/02-template-string';
-
+//import './bases/02-template-string';
+import './bases/01-const-let';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
